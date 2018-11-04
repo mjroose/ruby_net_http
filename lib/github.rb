2175bd1e5dbd2d6fe7008193d81c6a31beae6789
Michael Roose <mjroose@gmail.com>
2018-11-04 01:55:18 -0500

    Correct last commit - add files

--- lib/github.rb
+++ lib/github.rb
@@ -9,6 +9,7 @@ class Github
         @token = JSON.parse(file)["token"]
         @repo_name = get_repo_name(learnide_link)
         @new_repo_url = nil
+        @local_repo_path = nil
     end
 
     def get_repo_name(learnide_link)
@@ -26,13 +27,26 @@ class Github
         puts "Forking repo...."
         headers = { params: {access_token: @token} }
         res = RestClient.post("https://api.github.com/repos/learn-co-students/#{@repo_name}/forks", {}, headers)
+        res_json = JSON.parse(res.body)
+
         # res["owner"]["login"] == "mjroose"
-        @new_repo_url = res["html_url"] #== "https://github.com/mjroose/sinatra-fwitter-group-project-cb-000"
+        @new_repo_url = res_json["html_url"] #== "https://github.com/mjroose/sinatra-fwitter-group-project-cb-000"
         #res["parent"]["html_url"] == "https://github.com/learn-co-students/sinatra-fwitter-group-project-cb-000"
-        binding.pry
     end
 
-    def clone_repo(git_url)
-        
+    def clone_repo
+        @local_repo_path = "/home/michael/learn/#{@repo_name}"
+        clone_cmd = "git clone #{@new_repo_url}.git"
+        mv_cmd = "mv #{@repo_name} #{@local_repo_path}"
+        puts "Cloning repo...."
+        puts %x( #{clone_cmd} )
+        puts "Moving local folder to coding directory"
+        puts %x( #{mv_cmd} )
+    end
+
+    def launch_code
+        launch_cmd = "code #{@local_repo_path}"
+        puts "Launching Visual Studio Code...."
+        puts %x( #{launch_cmd} )
     end
 end
--- openide.rb
+++ openide.rb
@@ -5,6 +5,8 @@ learn_ide_link = ARGV[0]
 
 gh = Github.new(learn_ide_link)
 gh.fork_repo
+gh.clone_repo
+gh.launch_code
 
 # clone repo to local folder
 # launch VSCode to open local folder
